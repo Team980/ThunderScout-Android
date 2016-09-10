db172fd92894b3b151b3f393493700e97a24aab8
Luke Myers <19lmyers@gmail.com>
2016-09-10 15:10:57 -0700

    Fix crash

--- app/src/main/java/com/team980/thunderscout/match/SummaryFragment.java
+++ app/src/main/java/com/team980/thunderscout/match/SummaryFragment.java
@@ -4,18 +4,14 @@ package com.team980.thunderscout.match;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.AppCompatEditText;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.team980.thunderscout.R;
 import com.team980.thunderscout.data.ScoutData;
-import com.team980.thunderscout.data.enumeration.Defense;
 
 public class SummaryFragment extends Fragment {
 
@@ -29,9 +25,6 @@ public class SummaryFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        NestedScrollView teleopScroll = (NestedScrollView) view.findViewById(R.id.teleop_scrollView);
-        teleopScroll.setOnScrollChangeListener((ScoutActivity) getActivity());
     }
 
     @Override
