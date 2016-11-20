68c45a8675d69c10d4db4525ea9081cfb48536c1
Luke Myers <19lmyers@gmail.com>
2016-11-20 15:13:46 -0800

    Inflate layouts

--- app/src/main/java/com/team980/thunderscout/info/statistics/AverageTeamInfoFragment.java
+++ app/src/main/java/com/team980/thunderscout/info/statistics/AverageTeamInfoFragment.java
@@ -1,7 +1,18 @@
 package com.team980.thunderscout.info.statistics;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.team980.thunderscout.R;
 
 public class AverageTeamInfoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_average_team_info, container, false);
+    }
 
 }
