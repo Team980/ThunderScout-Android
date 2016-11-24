cc7df46e0ec4f252c0bd448c35a203222f8bb99c
Luke Myers <19lmyers@gmail.com>
2016-11-24 14:59:41 -0800

    CounterCompoundViews now save their instance state

--- app/src/main/java/com/team980/thunderscout/util/CounterCompoundView.java
+++ app/src/main/java/com/team980/thunderscout/util/CounterCompoundView.java
@@ -2,6 +2,8 @@ package com.team980.thunderscout.util;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -80,5 +82,88 @@ public class CounterCompoundView extends FrameLayout implements View.OnClickList
 
     public float getValue() {
         return value;
+    }
+
+    @Override
+    public Parcelable onSaveInstanceState() {
+        //begin boilerplate code that allows parent classes to save state
+        Parcelable superState = super.onSaveInstanceState();
+
+        SavedState ss = new SavedState(superState);
+        //end
+
+        ss.max = this.max;
+        ss.min = this.min;
+        ss.count = this.count;
+        ss.value = this.value;
+
+        return ss;
+    }
+
+    @Override
+    public void onRestoreInstanceState(Parcelable state) {
+        //begin boilerplate code so parent classes can restore state
+        if(!(state instanceof SavedState)) {
+            super.onRestoreInstanceState(state);
+            return;
+        }
+
+        SavedState ss = (SavedState)state;
+        super.onRestoreInstanceState(ss.getSuperState());
+        //end
+
+        this.max = ss.max;
+        this.min = ss.min;
+        this.count = ss.count;
+        this.value = ss.value;
+
+        String text;
+
+        if (Float.compare(value, Math.round(value)) != 0) {
+            text = Float.toString(value);
+        } else {
+            text = Integer.toString((int) value);
+        }
+
+        ((TextView) findViewById(R.id.value)).setText(text);
+    }
+
+    static class SavedState extends BaseSavedState {
+        float max;
+        float min;
+        float count;
+        float value;
+
+        SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        private SavedState(Parcel in) {
+            super(in);
+            this.max = in.readFloat();
+            this.min = in.readFloat();
+            this.count = in.readFloat();
+            this.value = in.readFloat();
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeFloat(this.max);
+            out.writeFloat(this.min);
+            out.writeFloat(this.count);
+            out.writeFloat(this.value);
+        }
+
+        //required field that makes Parcelables from a Parcel
+        public static final Parcelable.Creator<SavedState> CREATOR =
+                new Parcelable.Creator<SavedState>() {
+                    public SavedState createFromParcel(Parcel in) {
+                        return new SavedState(in);
+                    }
+                    public SavedState[] newArray(int size) {
+                        return new SavedState[size];
+                    }
+                };
     }
 }
