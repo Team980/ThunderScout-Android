22501ace52e573ae88036e87d2992d7726ee1c4f
Luke Myers <19lmyers@gmail.com>
2016-12-08 22:00:05 -0800

    Put the BREAKING dialog changes that make the app unusable in the unstable branch
    ...and don't forget to fix them eventually

---
+++ app/src/main/java/com/team980/thunderscout/match/ScoutingFlowDialogFragment.java
@@ -0,0 +1,85 @@
+package com.team980.thunderscout.match;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import com.team980.thunderscout.R;
+
+public class ScoutingFlowDialogFragment extends AppCompatDialogFragment {
+
+    /* The activity that creates an instance of this dialog fragment must
+     * implement this interface in order to receive event callbacks.
+     * Each method passes the DialogFragment in case the host needs to query it. */
+    public interface ScoutingFlowDialogFragmentListener {
+        public void onDialogPositiveClick(ScoutingFlowDialogFragment dialog);
+
+        public void onDialogNegativeClick(ScoutingFlowDialogFragment dialog);
+    }
+
+    // Use this instance of the interface to deliver action events
+    ScoutingFlowDialogFragmentListener mListener;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_scouting_flow, null))
+                // Add action buttons
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        mListener.onDialogPositiveClick(ScoutingFlowDialogFragment.this);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        mListener.onDialogNegativeClick(ScoutingFlowDialogFragment.this);
+                    }
+                });
+
+        final Dialog dialog = builder.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() { //Complex code to override auto dismiss
+
+            @Override
+            public void onShow(DialogInterface d) {
+
+                Button button = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        mListener.onDialogPositiveClick(ScoutingFlowDialogFragment.this);
+                    }
+                });
+            }
+        });
+        return dialog;
+    }
+
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the ScoutingFlowDialogFragmentListener so we can send events to the host
+            mListener = (ScoutingFlowDialogFragmentListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement ScoutingFlowDialogFragmentListener");
+        }
+    }
+
+}
