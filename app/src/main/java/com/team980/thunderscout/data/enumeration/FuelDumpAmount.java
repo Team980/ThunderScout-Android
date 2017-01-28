5cae0e299c5b92c5a229c53a633339245294d52d
Luke Myers <19lmyers@gmail.com>
2017-01-28 10:54:11 -0800

    Friendly strings

--- app/src/main/java/com/team980/thunderscout/data/enumeration/FuelDumpAmount.java
+++ app/src/main/java/com/team980/thunderscout/data/enumeration/FuelDumpAmount.java
@@ -2,18 +2,20 @@ package com.team980.thunderscout.data.enumeration;
 
 public enum FuelDumpAmount {
 
-    NONE(0, 0),
-    SMALL(1, 9),
-    MEDIUM(10, 29),
-    LARGE(30, 49),
-    EXTRA_LARGE(50, 70); //maximum amount low goal can hold
+    NONE("None", 0, 0),
+    SMALL("Small", 1, 9),
+    MEDIUM("Medium", 10, 29),
+    LARGE("Large", 30, 49),
+    EXTRA_LARGE("Extra Large", 50, 70); //maximum amount low goal can hold
 
+    private String displayString;
     private int minimumAmount;
     private int maximumAmount;
 
-    FuelDumpAmount(int par1, int par2) {
-        minimumAmount = par1;
-        maximumAmount = par2;
+    FuelDumpAmount(String par1, int min, int max) {
+        displayString = par1;
+        minimumAmount = min;
+        maximumAmount = max;
     }
 
     public int getMinimumAmount() {
@@ -40,7 +42,10 @@ public enum FuelDumpAmount {
         return FuelDumpAmount.NONE;
     }
 
-    //TODO average multiple FuelDumpAmounts
+    @Override
+    public String toString() {
+        return displayString;
+    }
 
-    //TODO override toString()
+    //TODO average multiple FuelDumpAmounts
 }
