f13bc8099d25d2973753c2094a673a16c164c9cd
Luke Myers <19lmyers@gmail.com>
2017-01-28 10:54:11 -0800

    Friendly strings

--- app/src/main/java/com/team980/thunderscout/data/enumeration/ClimbingStats.java
+++ app/src/main/java/com/team980/thunderscout/data/enumeration/ClimbingStats.java
@@ -2,7 +2,18 @@ package com.team980.thunderscout.data.enumeration;
 
 public enum ClimbingStats {
 
-    DID_NOT_CLIMB,
-    ATTEMPTED_CLIMB,
-    PRESSED_TOUCHPAD; //only option that scores
+    DID_NOT_CLIMB("Did not climb"),
+    ATTEMPTED_CLIMB("Attempted climb"),
+    PRESSED_TOUCHPAD("Pressed touchpad"); //only option that scores
+
+    private String displayString;
+
+    ClimbingStats(String par1) {
+        displayString = par1;
+    }
+
+    @Override
+    public String toString() {
+        return displayString;
+    }
 }
