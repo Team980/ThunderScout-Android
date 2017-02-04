4eb60e37bb5cdeda16042bd4a442fd370d5ff247
Luke Myers <19lmyers@gmail.com>
2017-02-04 10:59:22 -0800

    Implement the permissions check

--- app/src/main/java/com/team980/thunderscout/export/ExportActivity.java
+++ app/src/main/java/com/team980/thunderscout/export/ExportActivity.java
@@ -1,33 +1,66 @@
 package com.team980.thunderscout.export;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.team980.thunderscout.R;
 
-public class ExportActivity extends AppCompatActivity {
+public class ExportActivity extends AppCompatActivity implements View.OnClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_export);
 
-        //TODO actually code this activity
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_GRANTED) {
-            CSVExportTask exportTask = new CSVExportTask(this);
-            exportTask.execute();
-        } else {
-            //Request permission
-            ActivityCompat.requestPermissions(
-                    this,
-                    new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE },
-                    1
-            );
-            //TODO redo export
+        Button exportButton = (Button) findViewById(R.id.buttonExport);
+        exportButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.buttonExport) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                CSVExportTask exportTask = new CSVExportTask(this);
+                exportTask.execute();
+            } else {
+                //Request permission
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        1
+                );
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                CSVExportTask exportTask = new CSVExportTask(this);
+                exportTask.execute();
+            } else {
+                //Why would you ever deny the permission?
+                Toast.makeText(this, "Please manually grant the permission", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivityForResult(intent, 0);
+            }
         }
     }
 }
