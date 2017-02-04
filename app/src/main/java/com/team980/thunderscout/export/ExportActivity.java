821242f7d0c51eb2eea1954fcd68ff3caec8554b
Luke Myers <19lmyers@gmail.com>
2017-02-04 11:06:01 -0800

    Open folder button

--- app/src/main/java/com/team980/thunderscout/export/ExportActivity.java
+++ app/src/main/java/com/team980/thunderscout/export/ExportActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -23,8 +24,11 @@ public class ExportActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_export);
 
-        Button exportButton = (Button) findViewById(R.id.buttonExport);
-        exportButton.setOnClickListener(this);
+        Button buttonExport = (Button) findViewById(R.id.buttonExport);
+        buttonExport.setOnClickListener(this);
+
+        Button buttonOpenFolder = (Button) findViewById(R.id.buttonOpenFolder);
+        buttonOpenFolder.setOnClickListener(this);
 
     }
 
@@ -42,6 +46,20 @@ public class ExportActivity extends AppCompatActivity implements View.OnClickLis
                         new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                         1
                 );
+            }
+        } else if (v.getId() == R.id.buttonOpenFolder) {
+            Uri selectedUri = Uri.parse(Environment.getExternalStorageDirectory() + "/ThunderScout/");
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(selectedUri, "resource/folder");
+
+            if (intent.resolveActivityInfo(getPackageManager(), 0) != null)
+            {
+                startActivity(intent);
+            }
+            else
+            {
+                // if you reach this place, it means there is no any file
+                // explorer app installed on your device
             }
         }
     }
