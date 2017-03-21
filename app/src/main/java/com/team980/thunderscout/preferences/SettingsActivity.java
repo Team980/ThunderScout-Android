ff428b6fc8531534bc0d9c636a06108d59c471f5
Luke Myers <19lmyers@gmail.com>
2017-03-20 18:51:42 -0700

    Fix visual glitch in Bluetooth settings

--- app/src/main/java/com/team980/thunderscout/preferences/SettingsActivity.java
+++ app/src/main/java/com/team980/thunderscout/preferences/SettingsActivity.java
@@ -275,6 +275,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
+            bindPreferenceSummaryToValue(findPreference("bt_bt_server_device"));
         }
     }
 }
