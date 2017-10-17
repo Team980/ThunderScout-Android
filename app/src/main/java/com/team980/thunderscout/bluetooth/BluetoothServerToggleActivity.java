7610c8a1c941689be219b9240b583d83a336183d
Luke Myers <19lmyers@gmail.com>
2017-10-16 17:23:00 -0700

    Implement Bluetooth Server toggle app shortcut
    Also fixes the app shortcut icons on API 25

---
+++ app/src/main/java/com/team980/thunderscout/bluetooth/BluetoothServerToggleActivity.java
@@ -0,0 +1,34 @@
+package com.team980.thunderscout.bluetooth;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.team980.thunderscout.R;
+
+public class BluetoothServerToggleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Toggle Bluetooth server
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean currentValue = sharedPref.getBoolean(getResources().getString(R.string.pref_enable_bluetooth_server), false);
+        sharedPref.edit().putBoolean(getResources().getString(R.string.pref_enable_bluetooth_server), !currentValue).apply();
+
+        String actionString;
+        if (!currentValue) {
+            actionString = "enabled";
+        } else {
+            actionString = "disabled";
+        }
+
+        Toast.makeText(this, "Bluetooth Server " + actionString, Toast.LENGTH_SHORT).show();
+
+        finish();
+    }
+}
